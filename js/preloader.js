00edf5d632462212fb5562e496b121c476b689a4
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2022-05-06 01:44:51 +0200

    Delete js directory

--- js/preloader.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function() {
-    $('#status').delay(2500).fadeOut();
-    $('#preloader').delay(2500).fadeOut('slow');
-    $('body').delay(2500).css({ 'overflow' : 'visible' });
-});
