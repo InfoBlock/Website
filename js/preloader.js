0cfed747c86376249b661bcc19da3765afd798a5
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2022-05-05 22:44:03 +0200

    Add files via upload
    Added an Preloader and changed Field 4 to YouTube

---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+$(window).on('load', function() {
+    $('#status').delay(2500).fadeOut();
+    $('#preloader').delay(2500).fadeOut('slow');
+    $('body').delay(2500).css({ 'overflow' : 'visible' });
+});
