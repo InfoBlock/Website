761929bc80a718115636e9f70eac53c14b085a7a
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2022-06-26 00:32:02 +0200

    Add files via upload

---
+++ terminal/js/commands.js
@@ -0,0 +1,62 @@
+var youtube = "https://youtube.com/channel/UCpxPcXv3eD8U3qEbcPEAVrQ";
+var twitter = "https://twitter.com/InfoBlock4";
+var password = "supersecretblockypassword";
+var instagram = "https://instagram.com/infoblock_ets/";
+var github = "https://github.com/InfoBlock";
+var email = "contact@infoblock.gq";
+
+whois = [
+    "<br>",
+    "Hey there, I'm InfoBlock! 👋",
+    "Hobby Developer and Content Creator.",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">InfoBlock' + "</a>",
+    'twitter        <a href="' + twitter + '" target="_blank">InfoBlock4' + '</a>',
+    'instagram      <a href="' + instagram + '" target="_blank">infoblock3' + '</a>',
+    'github         <a href="' + github + '" target="_blank">InfoBlock' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+  
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is InfoBlock?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">video</span>          View YouTube videos',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">© InfoBlock 2022. All rights reserved.</span>',
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
